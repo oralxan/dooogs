668b75f7e71cd426fe914d6d0fb4fad263de5209
oralxan <oralxan@users.noreply.github.com>
2024-01-08 22:29:49 +0500

    new com

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,12 @@
 #from django.views.generic import path
 from django.urls import path
 
-from .views import HomePage,Posts,Post
+from .views import HomePage,PostList,PostDetail,PostCreate
 
 urlpatterns = [
     path('',HomePage.as_view(),name='home'),
-    path('posts/',Posts.as_view(),name = 'posts'),
-    path('posts/<int:pk>/',Post.as_view(),name= 'post')
+    path('posts/',PostList.as_view(),name = 'posts'),
+    path('posts/<int:pk>/',PostDetail.as_view(),name= 'post'),
+    path('posts/create/', PostCreate.as_view(),name='post_create')
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,25 @@
-from django.views.generic import TemplateView,ListView,DetailView
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    CreateView
+)
 from .models import Post
 
 class HomePage(TemplateView):
     template_name = 'pages/home.html'
-class Posts(ListView):
+class PostList(ListView):
     model=Post
     template_name = 'pages/posts.html'
-    context_object_name = 'posts'
-class Post(DetailView):
+    context_object_name = 'post_list'
+class PostDetail(DetailView):
     model = Post
     template_name = 'pages/detail.html'
-    context_object_name = 'post'
+    context_object_name = 'post_datail'
+
+class PostCreate(CreateView):
+    model = Post
+    template_name = 'pages/create.html'
+    content_type = 'post_create'
+    fields = ['title','description','image']
+
